9225ff9f12836ca0578ea92c87d9ca35f8dc843e
LydiaHaile <lydiaefrem@gmail.com>
2022-11-24 14:39:52 -0500

    update gitignore, change permissions orientdb_test.sh, upload results folder

--- orientdb_test.sh
+++ orientdb_test.sh
@@ -10,15 +10,15 @@ cd ../ycsb-0.17.0
 # run orientdb three times on workload a - save output in directory results/orientdb/workload_A
 for iter in {1..3}
     do
-        bin/ycsb.sh load orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../../results/orientdb/workload_A/output_load${iter}
-        bin/ycsb.sh run orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../../results/orientdb/workload_A/output_run${iter}
+        bin/ycsb.sh load orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results/orientdb/workload_A/output_load${iter}
+        bin/ycsb.sh run orientdb -P workloads/workloada -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results/orientdb/workload_A/output_run${iter}
     done
 
 # run orientdb three times on workload b – save output in directory results/orientdb/workload_B
 for iter in {1..3}
     do
-        bin/ycsb.sh load orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../../results/orientdb/workload_B/output_load${iter}
-        bin/ycsb.sh run orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../../results/orientdb/workload_B/output_run${iter}
+        bin/ycsb.sh load orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results/orientdb/workload_B/output_load${iter}
+        bin/ycsb.sh run orientdb -P workloads/workloadb -p orientdb.url=plocal:localhost:2480 -p orientdb.password=admin > ../results/orientdb/workload_B/output_run${iter}
     done
 
 cd ../targetDB
